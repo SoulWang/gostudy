a79d775a77fb1b75c1e20152453987d572d27b59
SoulWang <442054828@qq.com>
2021-05-17 15:20:05 +0800

    内嵌的使用

--- tree/entry/entry.go
+++ tree/entry/entry.go
@@ -18,32 +18,41 @@ go语言没有class，只有struct
 */
 
 type myTreeNode struct {
-	node *tree.Node
+	//node *tree.Node
+	//Embedding 内嵌
+	*tree.Node
 }
 
 func (myNode *myTreeNode) postOrder() {
-	if myNode == nil || myNode.node == nil {
+	if myNode == nil || myNode.Node == nil {
 		return
 	}
-	left := myTreeNode{myNode.node.Left}
+	//left := myTreeNode{myNode.Node.Left}
+	left := myTreeNode{myNode.Left}
 	left.postOrder()
-	right := myTreeNode{myNode.node.Right}
+	//right := myTreeNode{myNode.Node.Right}
+	right := myTreeNode{myNode.Right}
 	right.postOrder()
-	myNode.node.Print()
+	myNode.Print()
+}
+
+func (myNode *myTreeNode) Traverse() {
+	fmt.Println("this method is shadowed.")
 }
 
 func main() {
-	var root tree.Node
-	root = tree.Node{Value: 3}
+	root := myTreeNode{&tree.Node{Value: 3}}
 	root.Left = &tree.Node{}
 	root.Right = &tree.Node{5, nil, nil}
 	root.Right.Left = new(tree.Node)
 	root.Left.Right = tree.CreateNode(2)
 	root.Right.Left.SetValue(4)
+	fmt.Print("In_order traversal: ")
 	root.Traverse()
+	root.Node.Traverse()
 
+	fmt.Print("My own poet_order traversal: ")
+	root.postOrder()
 	fmt.Println()
-	myRoot := myTreeNode{&root}
-	myRoot.postOrder()
-	fmt.Println()
+
 }
